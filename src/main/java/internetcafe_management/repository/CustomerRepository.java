551c68e73f8c83e0504aca40c033b148e2b2b54a
tuong1901 <hoanghoangcattuong@gmail.com>
2025-10-18 01:15:23 +0700

    add folder repposirity

---
+++ src/main/java/internetcafe_management/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package internetcafe_management.repository;
+
+import internetcafe_management.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    // Có thể thêm method tùy chỉnh, ví dụ:
+    // List<Customer> findByCustomerNameContaining(String name);
+}
