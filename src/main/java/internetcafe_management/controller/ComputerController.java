1af65f37ccd1382a06a44a92796e23adaba8caf8
dangtiendat24 <BIT230080@st.cmc-u.edu.vn>
2025-10-17 23:46:22 +0700

    add controller getAllComputer

--- src/main/java/internetcafe_management/controller/ComputerController.java
+++ src/main/java/internetcafe_management/controller/ComputerController.java
@@ -3,6 +3,8 @@ package internetcafe_management.controller;
 import internetcafe_management.dto.ComputerDTO;
 import internetcafe_management.service.computer.ComputerService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,12 @@ public class ComputerController {
 
     public ComputerController(ComputerService computerService) {
         this.computerService = computerService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ComputerDTO>> getAllComputers(Pageable pageable) {
+        Page<ComputerDTO> computers = computerService.getAllComputers(pageable);
+        return ResponseEntity.ok(computers);
     }
 
     @PostMapping
