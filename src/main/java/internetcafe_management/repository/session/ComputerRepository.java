1187cfd141b3e75c06b39ac0d08a6b07e2bc0571
dangtiendat24 <BIT230080@st.cmc-u.edu.vn>
2025-10-17 22:42:23 +0700

    create ComputerRepository

---
+++ src/main/java/internetcafe_management/repository/session/ComputerRepository.java
@@ -0,0 +1,12 @@
+package internetcafe_management.repository.session;
+
+import internetcafe_management.entity.Computer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface ComputerRepository extends JpaRepository<Computer, Integer> {
+
+    Optional<Computer> findByComputerName(String computerName);
+}
