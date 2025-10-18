561d9ecf77cfedcf43f3838a1cac8240e8ea82c9
Winnie Pham <142828503+phamthangph13@users.noreply.github.com>
2025-10-18 12:59:47 +0700

    feat: Add Swagger configuration class with API documentation setup

---
+++ src/main/java/internetcafe_management/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package internetcafe_management.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Internet Cafe Management API")
+                        .description("API documentation for Internet Cafe Management System")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("CMC Global Team")
+                                .email("support@cmcglobal.com")
+                                .url("https://github.com/CMC-Global-Team/cyberxarena-server"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080/api/v1")
+                                .description("Development server"),
+                        new Server()
+                                .url("https://cyberxarena-server.herokuapp.com/api/v1")
+                                .description("Production server")
+                ));
+    }
+}
