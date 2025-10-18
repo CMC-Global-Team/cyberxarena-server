f1b95db6cf94bfa9a69338a886948a53d5891a4b
Winnie Pham <142828503+phamthangph13@users.noreply.github.com>
2025-10-18 22:59:13 +0700

    upload session list (#23)
    Co-authored-by: pl1201 <152005792+pl1201@users.noreply.github.com>

---
+++ src/main/java/internetcafe_management/controller/SessionController.java
@@ -0,0 +1,28 @@
+package internetcafe_management.controller;
+
+import internetcafe_management.entity.Session;
+import internetcafe_management.service.session.SessionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/sessions")
+@RequiredArgsConstructor
+public class SessionController {
+
+    private final SessionService sessionService;
+
+    @GetMapping
+    public Page<Session> getAllSessions(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "startTime,asc") String[] sort) {
+
+        Sort.Direction direction = sort[1].equalsIgnoreCase("desc") ? Sort.Direction.DESC : Sort.Direction.ASC;
+        PageRequest pageable = PageRequest.of(page, size, Sort.by(direction, sort[0]));
+        return sessionService.getAllSessions(pageable);
+    }
+}
--- src/main/java/internetcafe_management/repository/session/SessionRepository.java
+++ src/main/java/internetcafe_management/repository/session/SessionRepository.java
@@ -1,10 +1,10 @@
 package internetcafe_management.repository.session;
 
 import internetcafe_management.entity.Session;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface SessionRepository extends JpaRepository<Session, Integer>, JpaSpecificationExecutor<Session> {
+public interface SessionRepository extends JpaRepository<Session, Integer> {
+    Page<Session> findAll(Pageable pageable);
 }
