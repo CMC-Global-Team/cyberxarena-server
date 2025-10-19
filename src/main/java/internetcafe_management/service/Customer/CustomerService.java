2e95546b87aa51c2b7b392665f0d4b538f2d40f6
tuong1901 <hoanghoangcattuong@gmail.com>
2025-10-19 19:37:40 +0700

    Add customer service add

---
+++ src/main/java/internetcafe_management/service/Customer/CustomerService.java
@@ -0,0 +1,9 @@
+package internetcafe_management.service.Customer;
+
+import internetcafe_management.dto.CustomerDTO;
+
+import java.util.List;
+
+public interface CustomerService {
+    CustomerDTO createCustomer(CustomerDTO dto);
+}
