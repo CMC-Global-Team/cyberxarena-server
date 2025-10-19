ded62a8f5accc99567f349df28dfee6d49b3a5e5
tuong1901 <hoanghoangcattuong@gmail.com>
2025-10-19 19:39:10 +0700

    Add customer serviceimpl add

---
+++ src/main/java/internetcafe_management/service/impl/Customer/CustomerServiceImpl.java
@@ -0,0 +1,30 @@
+package internetcafe_management.service.impl.Customer;
+
+import internetcafe_management.dto.CustomerDTO;
+import internetcafe_management.entity.Customer;
+import internetcafe_management.mapper.Customer.CustomerMapper;
+import internetcafe_management.repository.Customer.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import internetcafe_management.service.Customer.CustomerService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerMapper customerMapper;
+
+    @Override
+    public CustomerDTO createCustomer(CustomerDTO dto) {
+        System.out.println("Creating customer with DTO: " + dto); // Log DTO
+        Customer entity = customerMapper.toEntity(dto);
+        Customer saved = customerRepository.save(entity);
+        return customerMapper.toDTO(saved);
+    }
+}
