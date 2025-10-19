44a04ad21f1dd6802150da6b62804c83353bea46
Winnie Pham <142828503+phamthangph13@users.noreply.github.com>
2025-10-20 01:18:47 +0700

    feat: implement searchAccounts method in AccountServiceImpl with pagination and sorting

--- src/main/java/internetcafe_management/repository/account/AccountRepository.java
+++ src/main/java/internetcafe_management/repository/account/AccountRepository.java
@@ -2,12 +2,13 @@ package internetcafe_management.repository.account;
 
 import internetcafe_management.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface AccountRepository extends JpaRepository<Account, Integer> {
+public interface AccountRepository extends JpaRepository<Account, Integer>, JpaSpecificationExecutor<Account> {
     
     /**
      * Tìm account theo username
