b56e586351a7aab47dc188ca6f8ddb5d7c6e6660
Winnie Pham <142828503+phamthangph13@users.noreply.github.com>
2025-10-19 13:58:06 +0700

    update secure in config

--- src/main/java/internetcafe_management/controller/SessionController.java
+++ src/main/java/internetcafe_management/controller/SessionController.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/sessions")
+@RequestMapping("/api/v1/sessions")
 @RequiredArgsConstructor
 public class SessionController {
 
