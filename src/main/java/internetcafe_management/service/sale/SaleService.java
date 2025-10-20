138cc4a6851a6776c3b6303ef5594b77b3d681f5
Tran Thanh <tienthanh6042@gmail.com>
2025-10-21 03:25:42 +0700

    Create SaleService interface

---
+++ src/main/java/internetcafe_management/service/sale/SaleService.java
@@ -0,0 +1,8 @@
+package internetcafe_management.service.sale;
+
+import internetcafe_management.dto.SaleDTO;
+import internetcafe_management.entity.Customer;
+
+public interface SaleService {
+    SaleDTO create(SaleDTO dto);
+}
