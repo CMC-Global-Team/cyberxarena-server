8f009842722ce4f54f06ced5f365df2cac9546bd
3T90hZ <165739979+3T90hZ@users.noreply.github.com>
2025-10-20 21:07:58 +0700

    Remove foreign key constraint from sale_total
    Removed foreign key constraint from sale_total table.

--- database.sql
+++ database.sql
@@ -375,11 +375,6 @@ ALTER TABLE `sale`
   ADD CONSTRAINT `sale_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`),
   ADD CONSTRAINT `sale_ibfk_2` FOREIGN KEY (`discount_id`) REFERENCES `discount` (`discount_id`);
 
---
--- Các ràng buộc cho bảng `sale_total`
---
-ALTER TABLE `sale_total`
-  ADD CONSTRAINT `sale_total_ibfk_1` FOREIGN KEY (`sale_id`) REFERENCES `sale` (`sale_id`);
 
 --
 -- Các ràng buộc cho bảng `sale_detail`
