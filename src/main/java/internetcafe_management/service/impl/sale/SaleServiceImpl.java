0c33ad2221c1d96a1a1b0cf2cf9858a984b691e2
Tran Thanh <tienthanh6042@gmail.com>
2025-10-22 02:43:57 +0700

    Implement update method

--- src/main/java/internetcafe_management/service/impl/sale/SaleServiceImpl.java
+++ src/main/java/internetcafe_management/service/impl/sale/SaleServiceImpl.java
@@ -1,11 +1,13 @@
 package internetcafe_management.service.impl.sale;
 
 import internetcafe_management.dto.SaleDTO;
+import internetcafe_management.dto.UpdateSaleRequestDTO;
 import internetcafe_management.entity.Customer;
 import internetcafe_management.entity.Sale;
 import internetcafe_management.mapper.Customer.CustomerMapper;
 import internetcafe_management.mapper.sale.SaleMapper;
 import internetcafe_management.repository.Customer.CustomerRepository;
+import internetcafe_management.repository.discount.DiscountRepository;
 import internetcafe_management.repository.sale.SaleRepository;
 import internetcafe_management.service.Customer.CustomerService;
 import internetcafe_management.service.sale.SaleService;
@@ -23,10 +25,38 @@ public class SaleServiceImpl implements SaleService {
     private final SaleRepository saleRepository;
     private final CustomerMapper customerMapper;
     private final CustomerService customerService;
+    private final CustomerRepository customerRepository;
+    private final DiscountRepository discountRepository;
     @Override
     public Sale create(SaleDTO dto, Integer customer) {
-
         Sale entity = saleMapper.toEntity(dto,customerMapper.toEntity(customerService.getCustomerById(customer)));
         return saleRepository.save(entity);
+    }
+
+    @Override
+    public Sale update(Integer saleId, UpdateSaleRequestDTO dto) {
+        Sale existingS = saleRepository.findById(saleId)
+                .orElseThrow(() -> new RuntimeException("Sale not found"));
+        if(dto.getDiscountId()!=null){
+            Customer existingC = customerRepository.findById(dto.getCustomerId())
+                    .orElseThrow(() -> new RuntimeException("Customer not found"));
+            existingS.setCustomer(existingC);
+        }
+        if(dto.getPaymentMethod() != null){
+            existingS.setPaymentMethod(dto.getPaymentMethod());
+        }
+        if(dto.getDiscountId()!=null) {
+            if(!discountRepository.existsById(dto.getDiscountId())){
+                throw new RuntimeException("Mã giảm giá '" + dto.getDiscountId() + "' không tồn tại");
+            }
+            existingS.setDiscountId(dto.getDiscountId());
+        }
+        if(dto.getPaymentMethod() != null){
+            existingS.setPaymentMethod(dto.getPaymentMethod());
+        }
+        if(dto.getNote() != null){
+            existingS.setNote(dto.getNote());
+        }
+        return null;
     }
 }
