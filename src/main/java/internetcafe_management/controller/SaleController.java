232b93bac3d9600a8e12b354c3f253d7782e73a8
Tran Thanh <tienthanh6042@gmail.com>
2025-10-22 02:58:01 +0700

    Update SaleController

--- src/main/java/internetcafe_management/controller/SaleController.java
+++ src/main/java/internetcafe_management/controller/SaleController.java
@@ -3,15 +3,9 @@ package internetcafe_management.controller;
 
 import internetcafe_management.dto.SaleDTO;
 import internetcafe_management.dto.UpdateSaleRequestDTO;
-import internetcafe_management.entity.Customer;
 import internetcafe_management.entity.Sale;
 import internetcafe_management.service.sale.SaleService;
-import internetcafe_management.service.session.SessionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,8 +19,15 @@ public class SaleController {
     public Sale createSession(@RequestBody SaleDTO sale, @RequestParam Integer customerId) {
         return saleService.create(sale, customerId);
     }
+
     @PutMapping("/{id}")
     public Sale updateSale(@PathVariable Integer id, @RequestBody UpdateSaleRequestDTO dto) {
         return saleService.update(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteComputer(@PathVariable Integer id) {
+        saleService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
