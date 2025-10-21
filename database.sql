444befcb92c5cb2c347e8356d08cc0b2a0aa63ae
Winnie Pham <142828503+phamthangph13@users.noreply.github.com>
2025-10-22 02:22:03 +0700

    Add 'is_default' field to membership_card table
    Added a new field 'is_default' to the membership_card table.

--- database.sql
+++ database.sql
@@ -52,6 +52,7 @@ CREATE TABLE membership_card (
   membership_card_id INT NOT NULL AUTO_INCREMENT,
   membership_card_name VARCHAR(100) NOT NULL,
   discount_id INT,
+  is_default BOOLEAN DEFAULT FALSE, -- Thêm field default
   PRIMARY KEY (membership_card_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
