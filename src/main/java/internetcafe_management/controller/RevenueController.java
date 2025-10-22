2734e18d1435eaf5031c20912d48a335fecec248
dangtiendat24 <BIT230080@st.cmc-u.edu.vn>
2025-10-22 21:46:53 +0700

    update RevenueController

--- src/main/java/internetcafe_management/controller/RevenueController.java
+++ src/main/java/internetcafe_management/controller/RevenueController.java
@@ -4,10 +4,13 @@ import internetcafe_management.dto.RevenueDTO;
 import internetcafe_management.service.revenue.RevenueService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/revenue")
@@ -23,5 +26,14 @@ public class RevenueController {
     public ResponseEntity<Page<RevenueDTO>> getAllRevenue(Pageable pageable) {
         Page<RevenueDTO> revenuePage = revenueService.getAllRevenue(pageable);
         return ResponseEntity.ok(revenuePage);
+    }
+
+    @PostMapping("/generate")
+    public ResponseEntity<List<RevenueDTO>> generateRevenueReports(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+
+        List<RevenueDTO> reports = revenueService.generateRevenueReports(startDate, endDate);
+        return new ResponseEntity<>(reports, HttpStatus.CREATED);
     }
 }
