da44b968d016ef9bbb7824bc1d56b93c17b9fae3
Tran Thanh <tienthanh6042@gmail.com>
2025-10-22 18:32:37 +0700

    Update SaleController

--- src/main/java/internetcafe_management/controller/SaleController.java
+++ src/main/java/internetcafe_management/controller/SaleController.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/sale")
 @RequiredArgsConstructor
@@ -16,18 +18,29 @@ public class SaleController {
     private final SaleService saleService;
 
     @PostMapping
-    public Sale createSession(@RequestBody SaleDTO sale, @RequestParam Integer customerId) {
+    public SaleDTO createSale(@RequestBody SaleDTO sale, @RequestParam Integer customerId) {
         return saleService.create(sale, customerId);
     }
 
     @PutMapping("/{id}")
-    public Sale updateSale(@PathVariable Integer id, @RequestBody UpdateSaleRequestDTO dto) {
+    public SaleDTO updateSale(@PathVariable Integer id, @RequestBody UpdateSaleRequestDTO dto) {
         return saleService.update(id, dto);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteComputer(@PathVariable Integer id) {
+    public ResponseEntity<Void> deleteSale(@PathVariable Integer id) {
         saleService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    // Tìm kiếm sale theo điều kiện
+    @GetMapping("/search")
+    public ResponseEntity<List<SaleDTO>> searchSale(
+            @RequestParam(required = false) String sortBy,
+            @RequestParam(required = false) String sortOrder,
+            @RequestParam(required = false) Integer saleId,
+            @RequestParam(required = false) Integer customerId,
+            @RequestParam(required = false) String customerName) {
+        return ResponseEntity.ok(saleService.searchSale(sortBy, sortOrder, saleId, customerId, customerName));
     }
 }
