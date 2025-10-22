a141e91ab14b9e9bbb16b71a9b14ff084f785daf
dangtiendat24 <BIT230080@st.cmc-u.edu.vn>
2025-10-22 21:03:24 +0700

    add existsByDate

--- src/main/java/internetcafe_management/repository/revenue/RevenueRepository.java
+++ src/main/java/internetcafe_management/repository/revenue/RevenueRepository.java
@@ -4,6 +4,9 @@ import internetcafe_management.entity.Revenue;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface RevenueRepository extends JpaRepository<Revenue, Integer> {
+    boolean existsByDate(LocalDate date);
 }
