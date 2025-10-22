4850d8dfd094f08adfe9e3384e98699598f82ae8
dangtiendat24 <BIT230080@st.cmc-u.edu.vn>
2025-10-22 20:25:36 +0700

    create RevenueController

---
+++ src/main/java/internetcafe_management/controller/RevenueController.java
@@ -0,0 +1,16 @@
+package internetcafe_management.controller;
+
+import internetcafe_management.dto.RevenueDTO;
+import internetcafe_management.service.revenue.RevenueService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/revenue")
+public class RevenueController {
+
+}
