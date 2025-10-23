46597ec86b13a9cbc11439ce750c743c085da4f5
Winnie Pham <142828503+phamthangph13@users.noreply.github.com>
2025-10-24 00:19:21 +0700

    feat: TĂ­ch há»£p stored procedure update_sale_total Ä‘á»ƒ tĂ­nh discount tá»± Ä‘á»™ng

--- src/main/java/internetcafe_management/repository/sale/SaleRepository.java
+++ src/main/java/internetcafe_management/repository/sale/SaleRepository.java
@@ -2,6 +2,7 @@ package internetcafe_management.repository.sale;
 
 import internetcafe_management.entity.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,4 +13,8 @@ public interface SaleRepository extends JpaRepository<Sale, Integer> {
     @Query("SELECT SUM(s.saleTotal.totalAmount) FROM Sale s " +
             "WHERE FUNCTION('DATE', s.saleDate) = :date")
     BigDecimal sumTotalAmountBySaleDate(@Param("date") LocalDate date);
+    
+    @Modifying
+    @Query(value = "CALL update_sale_total(:saleId)", nativeQuery = true)
+    void callUpdateSaleTotal(@Param("saleId") Integer saleId);
 }
