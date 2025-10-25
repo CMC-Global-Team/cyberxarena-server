b3e572a28b980a6179c505103e445225e8417b02
thienphuc5 <phucnguyenkid27@gmail.com>
2025-10-25 23:04:03 +0700

    Add Platinum membership card to mock data

--- Mock-data.sql
+++ Mock-data.sql
@@ -2,10 +2,10 @@ USE railway;
 
 INSERT INTO discount (discount_name, discount_type, discount_value) VALUES
 ('Premium Membership', 'Percentage', 5.00),
-('Platinum Membership', 'Percentage', 10.00),
+('Platinum Membership', 'Percentage', 10.00);
 
 INSERT INTO membership_card (membership_card_name, discount_id, recharge_threshold, is_default) VALUES
 ('Member', 1, 0.00, TRUE),     
 ('Premium ', 2, 10000000.00, FALSE),    
-  
+('Platinum ', 3, 15000000.00, FALSE);     
 
