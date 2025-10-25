28e55288729c7f01a023e6a8c1d597daeab0a99f
thienphuc5 <phucnguyenkid27@gmail.com>
2025-10-25 23:03:17 +0700

    Add membership card data to Mock-data.sql

--- Mock-data.sql
+++ Mock-data.sql
@@ -3,3 +3,8 @@ USE railway;
 INSERT INTO discount (discount_name, discount_type, discount_value) VALUES
 ('Premium Membership', 'Percentage', 5.00),
 ('Platinum Membership', 'Percentage', 10.00),
+
+INSERT INTO membership_card (membership_card_name, discount_id, recharge_threshold, is_default) VALUES
+('Member', 1, 0.00, TRUE),         
+  
+
