32394d116380311b2a6e30a2ad490b9e48710b72
thienphuc5 <phucnguyenkid27@gmail.com>
2025-10-25 23:03:34 +0700

    Add Premium membership card to mock data

--- Mock-data.sql
+++ Mock-data.sql
@@ -5,6 +5,7 @@ INSERT INTO discount (discount_name, discount_type, discount_value) VALUES
 ('Platinum Membership', 'Percentage', 10.00),
 
 INSERT INTO membership_card (membership_card_name, discount_id, recharge_threshold, is_default) VALUES
-('Member', 1, 0.00, TRUE),         
+('Member', 1, 0.00, TRUE),     
+('Premium ', 2, 10000000.00, FALSE),    
   
 
