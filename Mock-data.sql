c5be2613ea8d2a6ef32169131a2f0d3e84ddda5b
thienphuc5 <phucnguyenkid27@gmail.com>
2025-10-26 23:20:35 +0700

    Add new account entry in Mock-data.sql

--- Mock-data.sql
+++ Mock-data.sql
@@ -32,6 +32,7 @@ INSERT INTO recharge_history (customer_id, amount, recharge_date) VALUES
 (5, 100000,'2025-04-03 18:37:00'),
 (6, 300000,'2025-04-03 19:36:00'),
 (7, 90000, '2025-04-03 9:50:00'),
+(8, 40000, '2025-06-03 13:40:00'),
 INSERT INTO account (customer_id, username, password) VALUES
 (1, 'nguyenthienphuc', 'p123456'),
 (2, 'thangpham', 'thang123'),
