67b2e4ff03130f30a5bd03f864c628711d57d887
thienphuc5 <phucnguyenkid27@gmail.com>
2025-10-27 21:04:01 +0700

    Update Mock-data.sql

--- Mock-data.sql
+++ Mock-data.sql
@@ -1,6 +1,7 @@
 USE railway;
 
 INSERT INTO discount (discount_name, discount_type, discount_value) VALUES
+('No Discount', 'FLAT', 0.00),  
 ('Premium Membership', 'Percentage', 5.00),
 ('Platinum Membership', 'Percentage', 10.00);
 
